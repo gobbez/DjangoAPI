000cdfc4725d0ea0d1091af5603755ee71e5f319
gobbez <a.gobbez@hotmail.it>
2025-02-21 22:30:34 +0100

    Full game! Now you can use Postman to do GET and POST requests to play the game!

--- API/pythonquiz/readme.md
+++ API/pythonquiz/readme.md
@@ -6,9 +6,9 @@ Use Postman to do a Post request and enable Ollama to generate N questions about
 2. Open Postman and in the Body write a "times": int 
 3. (if in local) use this url http://127.0.0.1:8000/addpythonquiz/
 4. Now Ollama will create N rows in the PythonQuiz table!
-* Play Game *
+* <b>Play Game</b> *
 5. Use Postman (or browser) to send a GET request and start your study-game! This api call will pick a random question from the table
-6. In the Postman body create a Json format with:
+6. In the Postman send a POST request with the body as a Json format with:
 {
   "quizmode": "true",
   "username": "YOUR_USERNAME",
@@ -17,6 +17,23 @@ Use Postman to do a Post request and enable Ollama to generate N questions about
 }
 7. This will enable you to play the game, and get a right or wrong answer (with points)
 
+
+## GET functions
+You can do different GET functions passing the "ask" param, I list them below:
+<ul>
+<li><b><u>showusers</u></b> = Show all Users table</li>
+<li><b><u>showtotquiz</u></b> = Show the total quiz in the PythonQuiz table</li>
+<li><b><u>randomquiz</u></b> = Get a random quiz from the PythonQuiz table, even those you have already answered</li>
+<li><b><u>newquiz</u></b> = Get a random quiz that you haven't answered yet</li>
+<li><b><u>wrongquiz</u></b> = Get a random quiz from those that you have it wrong</li>
+</ul>
+
+## POST functions
+You can do different POST functions passing the "quizmode" param, I list them below:
+<ul>
+<li><b><u>true</u></b> = Enable the game. You can answer a question (use one of the above GET functions to see the question!). In this mode the answer will be saved in the right or wrong Users column to enable "newquiz" or "wrongquiz" GET functions.</li>
+<li><b><u>not true</u></b> = Make Ollama create N questions in the PythonQuiz table (use "times" param to say how many questions to add!)</li>
+</ul>
+
 Enjoy!
 
-PS: Further improvements would be to add another table to save your answers/points or wrong answers..
--- API/pythonquiz/views.py
+++ API/pythonquiz/views.py
@@ -17,22 +17,85 @@ class PythonQuizView(View):
 
     def get(self, request):
         """Get a random question"""
-        random_question = PythonQuiz.objects.order_by("?").first()
-        if not random_question:
-            return JsonResponse({"error": "No questions available"}, status=404)
+        data = json.loads(request.body.decode('utf-8'))
+        ask = data.get("ask")
 
-        return JsonResponse(
-            {"Quiz": {
-                "id": random_question.id,
-                "question": random_question.question,
-                "A": random_question.A,
-                "B": random_question.B,
-                "C": random_question.C,
-                "D": random_question.D,
-                # "correct": random_question.correct,
-                "points": random_question.points
-            }}
-        )
+        if ask == "showusers":
+            # Show the user table
+            all_users = Users.objects.values()
+            return JsonResponse({"Users": list(all_users)})
+
+        elif ask == "showtotquiz":
+            # Show the number of questions in the PythonQuiz table
+            all_records = PythonQuiz.objects.values()
+            return JsonResponse({"Number of questions": len(list(all_records))})
+
+        elif ask == "randomquiz":
+            # Get a random question
+            random_question = PythonQuiz.objects.order_by("?").first()
+            if not random_question:
+                return JsonResponse({"error": "No questions available"}, status=404)
+
+            return JsonResponse(
+                {"Quiz": {
+                    "id": random_question.id,
+                    "question": random_question.question,
+                    "A": random_question.A,
+                    "B": random_question.B,
+                    "C": random_question.C,
+                    "D": random_question.D,
+                    # "correct": random_question.correct,
+                    "points": random_question.points
+                }}
+            )
+
+        elif ask == "newquiz":
+            # Filter only questions that you haven't answered yet
+            username = data.get("username")
+            positive_answers = Users.objects.filter(username=username).exclude(right=0).values_list("right", flat=True)
+            negative_answers = Users.objects.filter(username=username).exclude(wrong=0).values_list("wrong", flat=True)
+            answered_quiz_ids = set(positive_answers) | set(negative_answers)
+            unanswered_quizzes = PythonQuiz.objects.exclude(id__in=answered_quiz_ids)
+            # Get a random question
+            random_question = unanswered_quizzes.order_by("?").first()
+            if not random_question:
+                return JsonResponse({"error": "No questions available"}, status=404)
+
+            return JsonResponse(
+                {"Quiz": {
+                    "id": random_question.id,
+                    "question": random_question.question,
+                    "A": random_question.A,
+                    "B": random_question.B,
+                    "C": random_question.C,
+                    "D": random_question.D,
+                    # "correct": random_question.correct,
+                    "points": random_question.points
+                }}
+            )
+
+        elif ask == "wrongquiz":
+            # Filter only questions that you have wrong
+            username = data.get("username")
+            negative_answers_ids = Users.objects.filter(username=username).exclude(wrong=0).values_list("wrong", flat=True)
+            negative_filter = PythonQuiz.objects.filter(id__in=negative_answers_ids)
+            # Get a random question
+            random_question = negative_filter.order_by("?").first()
+            if not random_question:
+                return JsonResponse({"error": "No questions available"}, status=404)
+
+            return JsonResponse(
+                {"Quiz": {
+                    "id": random_question.id,
+                    "question": random_question.question,
+                    "A": random_question.A,
+                    "B": random_question.B,
+                    "C": random_question.C,
+                    "D": random_question.D,
+                    # "correct": random_question.correct,
+                    "points": random_question.points
+                }}
+            )
 
     @csrf_exempt
     def post(self, request):
@@ -50,9 +113,19 @@ class PythonQuizView(View):
 
                 # Check if your answer is correct
                 if answer == solution:
+                    record = Users.objects.get(wrong=quest_id)
+                    if record:
+                        # Delete the row of the wrong answer in the Users table
+                        record.delete()
+                    # Save the question id in the right column of Users
                     Users.objects.create(username=username, right=quest_id, wrong=0)
                     return JsonResponse({"success": f"Great job! You won: {points} points!"}, status=200)
                 else:
+                    record = Users.objects.get(right=quest_id)
+                    if record:
+                        # Delete the row of the right answer in the Users table
+                        record.delete()
+                    # Save the question id in the wrong column of Users
                     Users.objects.create(username=username, wrong=quest_id, right=0)
                     return JsonResponse({"failed": f"Wrong! Try again!"}, status=200)
 
